9b79d6e8a0bb577b905a6d63a1d21e55adfe789b
Jonathan Bartlett <jonathan@jonnobrow.co.uk>
2021-04-12 10:36:19 +0100

    Neovim: Vimtex: Forward Search and Zathura

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -61,9 +61,7 @@ local function init()
     use {'mhartington/formatter.nvim', config = [[require('config.format')]]}
 
     -- LaTeX
-    use {
-        'lervag/vimtex' -- config = [[require('config.vimtex')]]
-    }
+    use {'lervag/vimtex', config = [[require('config.vimtex')]]}
 
     -- Git
     use {
