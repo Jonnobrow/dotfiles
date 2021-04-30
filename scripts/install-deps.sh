fbda4b4ec4e051a9f3c115f8836f72544892b641
Jonathan Bartlett <jonathan@jonnobrow.co.uk>
2021-04-30 11:35:14 +0100

    Updated dependency files

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -6,11 +6,13 @@ DIR=$(dirname "$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)")
 . "$DIR/scripts/ansi"
 
 PACMAN_BUNDLE_FILE="$DIR/Pacfile"
+PACMAN_BUNDLE=$(cat $PACMAN_BUNDLE_FILE | grep -v "#" | tr "\n" " ")
+
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 	# ask sudo upfront
 	sudo -v
 	sudo pacman -Syy
 	ansi --green "Using $PACMAN_BUNDLE_FILE bundle file"
-	sudo pacman -S --noconfirm --needed - <"$PACMAN_BUNDLE_FILE"
+	sudo pacman -S --noconfirm --needed $PACMAN_BUNDLE
 fi
