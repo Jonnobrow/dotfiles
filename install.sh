7ae5738f2fd639b0207910c6377705bb43a5c37a
Jonathan Bartlett <jonathan@jonnobrow.co.uk>
2022-01-08 15:58:48 +0000

    Add safer aliasing, sourcing for zsh

--- install.sh
+++ install.sh
@@ -3,18 +3,18 @@
 set -e
 
 if [ ! "$(command -v chezmoi)" ]; then
-  bin_dir="$HOME/.local/bin"
-  chezmoi="$bin_dir/chezmoi"
-  if [ "$(command -v curl)" ]; then
-    sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$bin_dir"
-  elif [ "$(command -v wget)" ]; then
-    sh -c "$(wget -qO- https://git.io/chezmoi)" -- -b "$bin_dir"
-  else
-    echo "To install chezmoi, you must have curl or wget installed." >&2
-    exit 1
-  fi
+	bin_dir="$HOME/.local/bin"
+	chezmoi="$bin_dir/chezmoi"
+	if [ "$(command -v curl)" ]; then
+		sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$bin_dir"
+	elif [ "$(command -v wget)" ]; then
+		sh -c "$(wget -qO- https://git.io/chezmoi)" -- -b "$bin_dir"
+	else
+		echo "To install chezmoi, you must have curl or wget installed." >&2
+		exit 1
+	fi
 else
-  chezmoi=chezmoi
+	chezmoi=chezmoi
 fi
 
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
