b75888a6caf39025a7f894e4518bf6222eab26c4
Jonathan Bartlett <jonathan@jonnobrow.co.uk>
2024-11-13 10:38:07 +0000

    Lazyvim and aliases

--- dot_config/nvim/lua/plugins/programming.lua
+++ dot_config/nvim/lua/plugins/programming.lua
@@ -1,23 +1,35 @@
 return {
-  -- Include some extra config from LazyVim
+	-- Include some extra config from LazyVim
 
-  -- -- Languages
-  { import = "lazyvim.plugins.extras.lang.typescript" },
-  { import = "lazyvim.plugins.extras.lang.json" },
-  { import = "lazyvim.plugins.extras.lang.terraform" },
-  { import = "lazyvim.plugins.extras.lang.tex" },
-  { import = "lazyvim.plugins.extras.lang.python" },
-  { import = "lazyvim.plugins.extras.lang.markdown" },
-  { import = "lazyvim.plugins.extras.lang.helm" },
-  { import = "lazyvim.plugins.extras.lang.go" },
-  { import = "lazyvim.plugins.extras.lang.git" },
-  { import = "lazyvim.plugins.extras.lang.ansible" },
-  { import = "lazyvim.plugins.extras.lang.yaml" },
-  { import = "lazyvim.plugins.extras.lang.ruby" },
+	-- -- Languages
+	{ import = "lazyvim.plugins.extras.lang.typescript" },
+	{ import = "lazyvim.plugins.extras.lang.json" },
+	{ import = "lazyvim.plugins.extras.lang.terraform" },
+	{ import = "lazyvim.plugins.extras.lang.tex" },
+	{ import = "lazyvim.plugins.extras.lang.python" },
+	{ import = "lazyvim.plugins.extras.lang.markdown" },
+	{ import = "lazyvim.plugins.extras.lang.helm" },
+	{ import = "lazyvim.plugins.extras.lang.go" },
+	{ import = "lazyvim.plugins.extras.lang.git" },
+	{ import = "lazyvim.plugins.extras.lang.ansible" },
+	{ import = "lazyvim.plugins.extras.lang.yaml" },
+	{ import = "lazyvim.plugins.extras.lang.ruby" },
 
-  -- -- Chezmoi
-  { import = "lazyvim.plugins.extras.util.chezmoi" },
+	-- -- Testing and Debugging
+	{ import = "lazyvim.plugins.extras.dap.core" },
+	{ import = "lazyvim.plugins.extras.test.core" },
+	{ "nvim-neotest/neotest-python" },
+	{
+		"nvim-neotest/neotest",
+		opts = {
+			adapters = { "neotest-python" },
+		},
+	},
 
-  -- Github Copilot
-  -- { import = "lazyvim.plugins.extras.coding.copilot" },
+	-- -- Chezmoi
+	{ import = "lazyvim.plugins.extras.util.chezmoi" },
+
+	-- Github Copilot
+	{ import = "lazyvim.plugins.extras.coding.copilot" },
+	{ import = "lazyvim.plugins.extras.coding.copilot-chat" },
 }
