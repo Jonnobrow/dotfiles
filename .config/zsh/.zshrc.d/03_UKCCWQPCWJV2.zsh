f6cab325b94c3ef91f8a8535fa5f4a74b1893f83
Jonathan Bartlett <jonathan@jonnobrow.co.uk>
2025-07-10 08:53:38 +0100

    fix: update work laptop config

--- .config/zsh/.zshrc.d/03_UKCCWQPCWJV2.zsh
+++ .config/zsh/.zshrc.d/03_UKCCWQPCWJV2.zsh
@@ -1,16 +1,40 @@
 # Work Laptop
 if [[ "$(hostname)" == "UKCCWQPCWJV2" ]]; then
   
-  # Aliases for samlaz
-  alias samlazdev='samlaz -e skycloud-gtvd-res-sds-dev -r skycloud-az-residentAdmins-dev && export AWS_PROFILE="skycloud-gtvd-res-sds-dev:skycloud-az-residentAdmins-dev" && export AWS_SHARED_CREDENTIALS_FILE=~/.aws/credentials'
-  alias samlazmgmt='samlaz -e skycloud-gtvd-res-sds-mgmt -r skycloud-az-residentAdmins-mgmt && export AWS_PROFILE="skycloud-gtvd-res-sds-mgmt:skycloud-az-residentAdmins-mgmt" && export AWS_SHARED_CREDENTIALS_FILE=~/.aws/credentials'
-  alias samlaznonp='samlaz -e skycloud-sds-res-uk-nonp -r skycloud-az-residentAdmins-nonp && export AWS_PROFILE="skycloud-sds-res-uk-nonp:skycloud-az-residentAdmins-nonp" && export AWS_SHARED_CREDENTIALS_FILE=~/.aws/credentials'
-  alias samlazprod='samlaz -e skycloud-sds-res-uk-prod -r skycloud-az-residentAdmins-prod && export AWS_PROFILE="skycloud-sds-res-uk-prod:skycloud-az-residentAdmins-prod" && export AWS_SHARED_CREDENTIALS_FILE=~/.aws/credentials'
-
   # SP AWS Aliases
-  alias sp-mgmt-admin='export AWS_PROFILE="sp-mgmt-admin" && aws sso login'
-  alias sp-dev-admin='export AWS_PROFILE="sp-dev-admin" && aws sso login'
-  alias sp-stage-admin='export AWS_PROFILE="sp-stage-admin" && aws sso login'
-  alias sp-prod-read='export AWS_PROFILE="sp-prod-read" && aws sso login'
+  alias sp-dev-admin='export AWS_PROFILE="sp-dev-admin" && aws_login'
+  alias sp-stage-admin='export AWS_PROFILE="sp-stage-admin" && aws_login'
+  alias sp-prod-admin='export AWS_PROFILE="sp-prod-admin" && aws_login'
+  alias sp-test-admin='export AWS_PROFILE="sp-test-admin" && aws_login'
+  alias sp-regression-admin='export AWS_PROFILE="sp-regression-admin" && aws_login'
+  alias sp-mgmt-admin='export AWS_PROFILE="sp-mgmt-admin" && aws_login'
+  
+  # Random String
+  randstr() {
+    len=${1:-16}
+    tr -dc A-Za-z0-9 </dev/urandom | head -c $len; echo
+  }
+  
+  aws_login() {
+    SSO_ACCOUNT=$(aws sts get-caller-identity --query "Account")
+    if ! [ ${#SSO_ACCOUNT} -eq 14 ]; then
+      aws sso login
+    fi
+  }
+  
+  codeartifact_login() {
+    sp-mgmt-admin
+    export CODEARTIFACT_AUTH_TOKEN=$(aws codeartifact \
+      get-authorization-token \
+      --domain sky-protect \
+      --domain-owner 872609768854 \
+      --region eu-west-1 \
+      --query authorizationToken \
+      --output text)
+  
+    bundle config https://sky-protect-872609768854.d.codeartifact.eu-west-1.amazonaws.com/ruby/gems/ aws:${CODEARTIFACT_AUTH_TOKEN}
+    export BUNDLE_SKY___PROTECT___872609768854__D__CODEARTIFACT__EU___WEST___1__AMAZONAWS__COM="aws:${CODEARTIFACT_AUTH_TOKEN}"
+    aws codeartifact login --tool pip --repository pips --domain sky-protect --domain-owner 872609768854 --region eu-west-1
+  }
 
 fi
