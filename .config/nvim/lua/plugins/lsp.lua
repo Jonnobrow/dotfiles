7879f7bb027eadec9bd53e96d9a588f209e14062
Jonathan Bartlett <jonathan@jonnobrow.co.uk>
2025-09-10 20:12:40 +0100

    chore: pkgs, nvim lock

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -5,4 +5,14 @@ return {
 			latex = { enabled = false },
 		},
 	},
+	{
+		"amitds1997/remote-nvim.nvim",
+		version = "*", -- Pin to GitHub releases
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- For standard functions
+			"MunifTanjim/nui.nvim", -- To build the plugin UI
+			"nvim-telescope/telescope.nvim", -- For picking b/w different remote methods
+		},
+		config = true,
+	},
 }
